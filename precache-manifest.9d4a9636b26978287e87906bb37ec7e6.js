eea00c41d2bd22abd565f3407fceef64b8c5ac87
oznerek <oznerek@gmail.com>
2019-08-07 11:09:48 +0200

    Updates

---
+++ precache-manifest.9d4a9636b26978287e87906bb37ec7e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4ee581c52fdeaf3be0e94bf9332a8c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "abf4200ac9bfe1065499",
+    "url": "/portfolio/static/js/2.0afbdaa9.chunk.js"
+  },
+  {
+    "revision": "f07a4f31bc16a2f7ccaa",
+    "url": "/portfolio/static/js/main.48f3144d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.42a77a0779316893ac9cf738d5c8efb8.js"
+  "/portfolio/precache-manifest.9d4a9636b26978287e87906bb37ec7e6.js"
 );
 
 self.addEventListener('message', (event) => {
